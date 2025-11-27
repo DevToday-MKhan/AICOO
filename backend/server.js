a6d6bbec7e870279885bb2134160fd5b7e1e59af
DevToday-MKhan <itschickentoday@gmail.com>
2025-11-27 00:18:05 +0000

    Fix path-to-regexp error in catch-all route by using regex pattern

--- backend/server.js
+++ backend/server.js
@@ -960,7 +960,7 @@ app.get("/api/test", (req, res) => {
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
